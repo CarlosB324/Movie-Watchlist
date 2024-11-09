ace2bebff24bb1ca4005afabc1f1de5d4d505868
CarlosB324 <127149062+CarlosB324@users.noreply.github.com>
2024-11-08 20:08:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,140 @@
+const searchBar = document.getElementById("search-bar")
+const movieInfoSection = document.getElementById("movie-info-section")
+const watchlistInfoSection = document.getElementById("watchlist-info-section")
+let movieArray = []
+let watchlistArray = []
+
+
+/* Handle Button Click */
+document.addEventListener('click', handleClick)
+
+function handleClick(e) {
+    e.preventDefault()
+    if (e.target.id === "search-btn") {
+        movieSearch()
+        searchBar.value = ""
+    } else if (e.target.dataset.watchlist) {
+        getMovieIndex(e.target.dataset.watchlist)
+    } else if (e.target.id === "to-watchlist") {
+        window.location="watchlist.html"
+    }
+}
+
+/* Getting Movie Info Section */
+function movieSearch() {
+    fetch(`https://www.omdbapi.com/?s=${searchBar.value}&apikey=c6e04130`)
+        .then(res => res.json())
+        .then(data => {
+            movieArray.splice(0, movieArray.length)
+            try {
+                getMovieInfo(data)
+                searchBar.placeholder = "Search for a movie"
+            }
+            catch(err) {
+                searchBar.placeholder = "Searching something with no data"
+                movieInfoSection.innerHTML = `
+                    <h3 class="fail">
+                    Unable to find what you’re looking for. Please try another search.
+                    </h3>
+                `
+            }
+        }) 
+}
+
+function displayImage(poster) {
+    if (poster == "N/A" ) {
+        return "images/No-Image-Placeholder.png"
+    } 
+    return poster
+}
+
+function getMovieInfo(e) {
+    let movies = e.Search
+    
+    for (movie of movies) {
+        fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=c6e04130`)
+            .then(res => res.json())
+            .then(movieData => {
+                
+                let movieObj = {
+                    id: movieData.imdbID,
+                    title: movieData.Title,
+                    rating: movieData.imdbRating,
+                    runtime: movieData.Runtime,
+                    genre: movieData.Genre,
+                    plot: movieData.Plot,
+                    poster: movieData.Poster,
+                    type: movieData.Type,
+                    year: movieData.Year
+                }
+                movieArray.push(movieObj) 
+                movieInfoRender()
+            })
+    }
+}
+
+function getMovieHtml() {
+    let movieHtml = ``
+    
+    movieArray.map((movieData) => {
+        movieHtml += `
+            <div class="movie-info" id="${movieData.id}">
+                <img src="${displayImage(movieData.poster)}">
+                <div class="info-section">
+                    <div class="name">
+                        <h3 class="title">${movieData.title}</h3>
+                    </div>
+                    <div class="rating-year-type">
+                        <p><i class="fa-sharp fa-solid fa-star"></i> ${movieData.rating}</p>
+                        <p>${movieData.year}</p>
+                        <p>${movieData.type}</p>
+                    </div>
+                    <div class="time-genre-watchlist">
+                        <p>${movieData.runtime}</p>
+                        <p>${movieData.genre}</p>
+                        <button id="watchlist" data-watchlist="${movieData.id}">
+                            <i class="fa-solid fa-circle-plus"></i> Watchlist</button>
+                    </div>
+                    <p class="gray">${movieData.plot}</p>
+                </div>
+            </div>
+            `
+    })
+    return movieHtml
+}
+
+function getMovieIndex(itemId) {
+    let index = movieArray.findIndex( item => item.id == itemId)
+    let watchlistIndex = watchlistArray.findIndex( item => item.id == itemId)
+    
+    if (watchlistIndex === -1) {
+        watchlistArray.push(movieArray[index])
+        document.getElementById("add-to-wl").style.opacity = 1
+        document.getElementById("add-to-wl").style.bottom = "40px"
+        setTimeout(function(){
+            document.getElementById("add-to-wl").style.opacity = 0
+            document.getElementById("add-to-wl").style.bottom = 0
+        }, 1500)
+    } else {
+        document.getElementById("already-in-wl").style.opacity = 1
+        document.getElementById("already-in-wl").style.bottom = "40px"
+        setTimeout(function(){
+            document.getElementById("already-in-wl").style.opacity = 0
+            document.getElementById("already-in-wl").style.bottom = 0
+        }, 1500)
+    }
+}
+
+/* Watchlist Local Storage Info */
+if (localStorage.getItem("movie")) {
+    watchlistArray = JSON.parse(localStorage.getItem('movie'))
+}
+
+window.addEventListener('beforeunload', function(e) {
+    localStorage.setItem('movie', JSON.stringify(watchlistArray))
+})
+
+/* Render Section */
+function movieInfoRender() {
+    movieInfoSection.innerHTML = getMovieHtml()
+}
---
+++ watchlist.js
@@ -0,0 +1,100 @@
+const watchlistInfoSection = document.getElementById("watchlist-info-section")
+let localWatchlist = []
+
+/* Getting Watchlist */
+if (localStorage.getItem("movie")) {
+    localWatchlist = JSON.parse(localStorage.getItem('movie'))
+}
+
+if (localWatchlist.length > 0) {
+    watchlistInfoRender()
+} else {
+     emptyWatchlistRender()
+}
+
+window.addEventListener('beforeunload', function(e) {
+    localStorage.setItem('movie', JSON.stringify(localWatchlist))
+})
+
+/* Handle Button Click */
+document.addEventListener('click', handleClick)
+
+function handleClick(e) {
+    e.preventDefault()
+    if (e.target.id === "to-movie-search") {
+        window.location="index.html"
+    } else if (e.target.id === "add-movies") {
+        window.location="index.html"
+    } else if (e.target.dataset.watchlist) {
+        removeBtn(e.target.dataset.watchlist)
+    }
+}
+
+/* Watchlist Info Section */
+function displayImage(poster) {
+    if (poster == "N/A" ) {
+        return "images/No-Image-Placeholder.png"
+    } 
+    return poster
+}
+
+function getWatchlistHtml() {
+    let watchlistHtml = ``
+    
+    localWatchlist.map((movieData) => {
+        watchlistHtml += `
+            <div class="movie-info" id="${movieData.id}">
+                <img src="${displayImage(movieData.poster)}">
+                <div class="info-section">
+                    <div class="name">
+                        <h3 class="title">${movieData.title}</h3>
+                    </div>
+                    <div class="rating-year-type">
+                        <p><i class="fa-sharp fa-solid fa-star"></i> ${movieData.rating}</p>
+                        <p>${movieData.year}</p>
+                        <p>${movieData.type}</p>
+                    </div>
+                    <div class="time-genre-watchlist">
+                        <p>${movieData.runtime}</p>
+                        <p>${movieData.genre}</p>
+                        <button id="watchlist" data-watchlist="${movieData.id}">
+                            <i class="fa-solid fa-circle-minus"></i> Remove</button>
+                    </div>
+                    <p class="gray">${movieData.plot}</p>
+                </div>
+            </div>
+            `
+    })
+    return watchlistHtml
+}
+
+function removeBtn(itemId) {
+    let index = localWatchlist.findIndex( item => item.id == itemId)
+    localWatchlist.splice(index, 1)
+    document.getElementById("remove-from-wl").style.opacity = 1
+    document.getElementById("remove-from-wl").style.bottom = "40px"
+    setTimeout(function(){
+        document.getElementById("remove-from-wl").style.opacity = 0
+        document.getElementById("remove-from-wl").style.bottom = 0
+    }, 1500)
+    if(localWatchlist.length === 0) {
+        emptyWatchlistRender()
+    } else {
+        watchlistInfoRender()
+    }
+}
+
+/* Render Section */
+function watchlistInfoRender() {
+    watchlistInfoSection.innerHTML = getWatchlistHtml()
+}
+
+function emptyWatchlistRender() {
+    watchlistInfoSection.innerHTML = `
+        <div class="watchlist-start">
+            <h3>Your watchlist is looking a little empty...</h3>
+            <button id="add-movies">
+                <i class="fa-solid fa-circle-plus"></i> Let’s add some movies!
+            </button>
+        </div>`
+}
